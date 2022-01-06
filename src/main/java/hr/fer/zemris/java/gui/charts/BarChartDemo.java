bfbaebbdee0952b19ca409eb88113bdba9ba5d7d
Leo Goršić <gorsicleo@gmail.com>
2022-01-06 13:53:38 +0100

    Code refactor and javadoc for BarChartDemo and XYValue

--- src/main/java/hr/fer/zemris/java/gui/charts/BarChartDemo.java
+++ src/main/java/hr/fer/zemris/java/gui/charts/BarChartDemo.java
@@ -1,33 +1,44 @@
 package hr.fer.zemris.java.gui.charts;
 
-import java.awt.BorderLayout;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+/**
+ * Class that demonstrates functionality of {@link BarChartComponent}. Class
+ * loads .txt file and constructs new {@link BarChart} and calls
+ * {@link BarChartComponent} to draw histogram.
+ * 
+ * @author gorsicleo
+ *
+ */
 @SuppressWarnings("serial")
 public class BarChartDemo extends JFrame {
 
+	private static final String WINDOW_TITLE = "Colorful Grapher";
+	/** {@link BarChart} containing histogram data */
 	private BarChart barChart;
 
+	/**
+	 * Creates new {@link BarChartDemo} and sets frame size to 500x500 size, then
+	 * calls methods to draw histogram
+	 */
 	public BarChartDemo(BarChart barChart) {
 		this.barChart = barChart;
 		initGUI();
 	}
 
-
+	/**Sets size, title and close operation for new Frame.
+	 * Constructs new {@link BarChartComponent} that will be added to frame.
+	 */
 	private void initGUI() {
 		setSize(500, 500);
-		setTitle("Grapher");
+		setTitle(WINDOW_TITLE);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
 		add(new BarChartComponent(barChart));
@@ -42,24 +53,24 @@ public class BarChartDemo extends JFrame {
 			String descriptionX = sc.nextLine();
 			String descriptionY = sc.nextLine();
 			List<XYValue> points = new LinkedList<XYValue>();
-			
+
 			String[] unparsedPoints = sc.nextLine().split(" ");
 			for (String point : unparsedPoints) {
 				int x = Integer.parseInt(point.split(",")[0]);
 				int y = Integer.parseInt(point.split(",")[1]);
 				points.add(new XYValue(x, y));
 			}
-			
+
 			int minY = Integer.parseInt(sc.nextLine());
 			int maxY = Integer.parseInt(sc.nextLine());
 			int step = Integer.parseInt(sc.nextLine());
-			
+
 			barChart = new BarChart(points, descriptionX, descriptionY, minY, maxY, step);
 			sc.close();
 		} catch (FileNotFoundException e) {
 			throw new IllegalArgumentException("can't open file!");
 		}
-		
+
 		SwingUtilities.invokeLater(() -> {
 			new BarChartDemo(barChart).setVisible(true);
 		});
--- src/main/java/hr/fer/zemris/java/gui/charts/XYValue.java
+++ src/main/java/hr/fer/zemris/java/gui/charts/XYValue.java
@@ -1,25 +1,33 @@
 package hr.fer.zemris.java.gui.charts;
 
+/**
+ * Simple model for a point in 2D space. Object is immutable once created.
+ * 
+ * @author gorsicleo
+ */
 public class XYValue {
-	
+
 	private final int x;
-	
+
 	private final int y;
-	
-	
+
+	/**creates new 2D point.
+	 * @param x
+	 * @param y
+	 */
 	public XYValue(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
+	/**Returns x coordinate of point.*/
 	public int getX() {
 		return x;
 	}
-	
+
+	/**Returns y coordinate of point.*/
 	public int getY() {
 		return y;
 	}
-	
-	
 
 }
